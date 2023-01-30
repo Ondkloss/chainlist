ea08f1884ba673068306ec82a299c133dd5bceb7
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2023-01-30 23:07:53 +0100

    Dark mode toggle. Light mode bugged appearance currently.

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -5,6 +5,14 @@ import { notTranslation as useTranslations } from "../../utils";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
 
+function darkify(e) {
+  e.preventDefault();
+  var element = document.body;
+  document.getElementById("theme-toggle-dark-icon").classList.toggle("hidden");
+  document.getElementById("theme-toggle-light-icon").classList.toggle("hidden");
+  element.classList.toggle("dark");
+}
+
 export default function Layout({ children, lang }) {
   const t = useTranslations("Common", lang);
 
@@ -78,6 +86,39 @@ export default function Layout({ children, lang }) {
               />
             </svg>
             <span className="text-base font-medium">{t("view-source-code")}</span>
+            <div>
+              <button
+                onClick={darkify}
+                id="theme-toggle"
+                type="button"
+                class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+              >
+                <svg
+                  id="theme-toggle-dark-icon"
+                  class="w-5 h-5"
+                  fill="#2F80ED"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+                  ></path>
+                </svg>
+                <svg
+                  id="theme-toggle-light-icon"
+                  class="w-5 h-5 hidden"
+                  fill="#2F80ED"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
           </a>
         </div>
       </div>
